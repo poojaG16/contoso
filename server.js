c847c7ae1b3e547f03b24d555165d6c724a2b932
pooja16 <poojagaikwad28603@gmail.com>
2022-02-21 17:48:25 +0530

    contoso application uploaded

---
+++ server.js
@@ -0,0 +1,18 @@
+var express =require("express");  //lib-----Node Module
+var app=express();  // will create app object from express package
+
+var onAboutUs=function(req, res){
+    res.send("Chief Mentor :Ravi Tambade");
+};
+var onDefault=function(req, res){
+    res.send("<h1>Contoso Pvt. Ltd</h1>"+
+              "<hr/>"+
+              "<h3>About Us</h3>"+
+              "<br>"+
+              "<h4>We have 12 Offices all Over the World.<h4>"
+             );
+};
+app.get("/",onDefault);  // Request handler functions are registered
+app.get("/aboutus",onAboutUs);  // Request handler functions are registered
+var server=app.listen(8081);
+console.log("Server is running on port 8081");
